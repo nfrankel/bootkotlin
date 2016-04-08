6a580a7bb866e7ecc9d59a8483d762406c8a663d
Nicolas Frankel <nicolas@frankel.ch>
2016-04-08 23:43:55 +0200

    Add a rest controller to show constructor and default values

---
+++ src/main/kotlin/ch/frankel/bootkotlin/WelcomeController.kt
@@ -0,0 +1,12 @@
+package ch.frankel.bootkotlin
+
+import org.springframework.web.bind.annotation.*
+
+@RestController
+class WelcomeController {
+
+    @RequestMapping("/welcome/{who}")
+    fun welcome(@PathVariable who: String) = Welcome(who)
+}
+
+class Welcome(val who: String, val what: String = "Welcome")
