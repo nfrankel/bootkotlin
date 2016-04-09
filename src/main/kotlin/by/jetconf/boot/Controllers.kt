166a5a60052402eebb84ddcb55035b81232fc765
Nicolas Frankel <nicolas@frankel.ch>
2016-04-09 09:38:37 +0200

    Refactored all controllers into the same file

--- src/main/kotlin/by/jetconf/boot/Controllers.kt
+++ src/main/kotlin/by/jetconf/boot/Controllers.kt
@@ -3,6 +3,20 @@ package by.jetconf.boot
 import org.springframework.web.bind.annotation.*
 
 @RestController
+class HelloController {
+
+    @RequestMapping("/hello")
+    fun sayHello() = "Hello JetConf"
+}
+
+@RestController
+class GreetingController {
+
+    @RequestMapping("/greet/{who}")
+    fun greet(@PathVariable who:String) = "Hello ${who}"
+}
+
+@RestController
 class WelcomeController {
 
     @RequestMapping("/welcome/{who}")
