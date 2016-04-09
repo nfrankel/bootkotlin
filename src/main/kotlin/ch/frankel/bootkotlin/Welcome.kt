0bff0253605c1f480a32e2e8d3e53b170f3d1e7f
Nicolas Frankel <nicolas@frankel.ch>
2016-04-09 13:59:43 +0200

    Add a new endpoint to show that Spring Data JPA auto-queries work

--- src/main/kotlin/ch/frankel/bootkotlin/Welcome.kt
+++ src/main/kotlin/ch/frankel/bootkotlin/Welcome.kt
@@ -2,8 +2,7 @@ package ch.frankel.bootkotlin
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.repository.CrudRepository
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.io.Serializable
 
 @RestController
@@ -11,12 +10,18 @@ class WelcomeController(private val service: WelcomeService) {
 
     @RequestMapping("/welcome")
     fun welcome() = service.getAll()
+
+    @RequestMapping("/welcome/{who}")
+    fun welcome(@PathVariable who: String) = service.get(who)
 }
 
 class WelcomeService(private val repository: WelcomeRepository) {
     fun getAll(): Iterable<Welcome> = repository.findAll()
+    fun get(who: String) = repository.findByWho(who)
 }
 
-interface WelcomeRepository : CrudRepository<Welcome, String>
+interface WelcomeRepository : CrudRepository<Welcome, String> {
+    fun findByWho(who: String): Welcome
+}
 
 class Welcome(@Id val who: String, val what: String = "Welcome") : Serializable
